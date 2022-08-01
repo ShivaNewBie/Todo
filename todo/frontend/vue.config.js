3c45a11aa35c91418d8057fffc27a6e2ca6d9395
Shiva <shivakumarsunderamurthy@gmail.com>
2022-08-01 22:29:30 +0800

    ADDED: Frontend vue.js

---
+++ todo/frontend/vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // configureWebpack: {
+  //   plugins: [new MiniCssExtractPlugin()],
+  // },
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/static/dist/"
+      : "http://127.0.0.1:8080",
+  outputDir: "../static/dist",
+  indexPath: "../../templates/index.html",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "todo",
+    },
+  },
+  devServer: {
+    devMiddleware: {
+      publicPath: "http://127.0.0.1:8080",
+      writeToDisk: (filePath) => filePath.endsWith("index.html"),
+    },
+    hot: "only",
+    headers: { "Access-Control-Allow-Origin": "*" },
+  },
+};
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -14,15 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from core.views import IndexTemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/', include('task.urls')),
     path("api-auth/", include("rest_framework.urls")),
-    
+
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
+    path('auth/', include('djoser.urls.authtoken')),
+
+    re_path(r"^.*$", IndexTemplateView.as_view(),name='entry-point') #entry point
 
 ]
